4407ae40e7a2ac864f1545c03eda7fd8421ef3e4
AntiqueR <antique.pendulum@gmail.com>
2020-10-02 18:28:19 +0900

    add BouncyCastle.dll and HashUtil.cs

---
+++ ArCana/Cryptography/HashUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Org.BouncyCastle.Crypto.Digests;
+
+namespace ArCana.Cryptography
+{
+    public static class HashUtil
+    {
+        public static byte[] RIPEMD160(byte[] data)
+        {
+            var digest = new RipeMD160Digest();
+            var result = new byte[digest.GetDigestSize()];
+            digest.BlockUpdate(data, 0, data.Length);
+            digest.DoFinal(result, 0);
+            return result;
+        }
+
+        public static byte[] SHA256(byte[] data)
+        {
+            using var sha256 = System.Security.Cryptography.SHA256.Create();
+            return sha256.ComputeHash(data);
+        }
+
+        public static byte[] DoubleSHA256Hash(byte[] data)
+        {
+            using var sha256 = System.Security.Cryptography.SHA256.Create();
+            return sha256.ComputeHash(sha256.ComputeHash(data));
+        }
+
+        public static byte[] Hash160(byte[] data) => RIPEMD160(SHA256(data));
+    }
+}
