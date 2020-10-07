26fbc6a98538aed9a31b6ef8b2733ab6bfc9d62d
AntiqueR <antique.pendulum@gmail.com>
2020-10-07 19:48:01 +0900

    add Block

---
+++ ArCana/Blockchain/Block.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArCana.Blockchain
+{
+    public class Block
+    {
+        public HexString Id { get; set; }
+        public uint Bits { get; set; }
+        public HexString PreviousBlockHash { get; set; }
+        public byte[] MerkleRootHash { get; set; }
+        public DateTime Timestamp { get; set; }
+        public ulong Nonce { get; set; }
+        public List<Transaction> Transactions { get; set; }
+    }
+}
