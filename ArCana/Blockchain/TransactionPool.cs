1bb7db9b808dbd6f70310496a10b71e9a035fb15
AntiqueR <antique.pendulum@gmail.com>
2020-10-13 13:48:49 +0900

    add TxPool and Test

---
+++ ArCana/Blockchain/TransactionPool.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArCana.Blockchain
+{
+    public class TransactionPool
+    {
+        public static TransactionPool Instance = new TransactionPool();
+
+        public List<Transaction> MemPool { get; } = new List<Transaction>();
+
+        TransactionPool(){ }
+
+        public Transaction[] GetPool() =>
+            MemPool.ToArray();
+
+        public void RemoveTxs(params HexString[] ids)
+        {
+            lock (MemPool)
+            {
+                var target = MemPool.Select(x => x.Id).Intersect(ids);
+                MemPool.RemoveAll(x => target.Contains(x.Id));
+            }
+        }
+
+        public void AddTxs(params Transaction[] txs)
+        {
+            lock (MemPool)
+            {
+                var exceptList = txs.Where(tx => !MemPool.Select(x => x.Id).Contains(tx.Id));
+                MemPool.AddRange(exceptList);
+            }
+        }
+    }
+}
