48ff0fc7eac019ba5b7ba33c6d570913afde7486
AntiqueR <antique.pendulum@gmail.com>
2020-10-22 21:06:48 +0900

    add Messages

---
+++ ArCana/Network/Messages/MessageType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArCana.Network.Messages
+{
+    public enum MessageType : byte
+    {
+        #region Server
+
+        HandShake = 0x00,
+        Addr = 0x01,
+
+        #endregion
+
+        #region Surface
+
+        SurfaceHandShake = 0x10,
+
+        #endregion
+
+        #region Transactions
+
+        Inventory = 0x20,
+        NewTransaction = 0x21,
+        NewBlock = 0x22,
+        RequestFullChain = 0x23,
+        FullChain = 0x24,
+
+        #endregion
+
+        #region Others
+
+        Ping = 0x30,
+        Notice = 0x31,
+
+        #endregion
+
+    }
+}
---
+++ ArCana/Network/Messages/Messages.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using ArCana.Blockchain;
+using ArCana.Extensions;
+using Utf8Json;
+using static Utf8Json.JsonSerializer;
+
+namespace ArCana.Network.Messages
+{
+    public class Message
+    {
+        public virtual MessageType Type { get; set; }
+
+        public virtual byte[] Payload { get; set; }
+
+        public async Task SendAsync(IPAddress remoteAddress, int port)
+        {
+            var timeOut = 3000;
+            using var client = new TcpClient()
+            {
+                SendTimeout = timeOut,
+                ReceiveTimeout = timeOut
+            };
+            await client.ConnectAsync(remoteAddress, port);
+            await using var stream = client.GetStream();
+            await SerializeAsync(stream, this);
+        }
+
+        public async Task SendMessageAsync(IPEndPoint remotePoint) =>
+            await SendAsync(remotePoint.Address, remotePoint.Port);
+    }
+
+    public class HandShake : IMessage
+    {
+        public List<string> KnowIpEndPoints { get; set; }
+
+        public Message ToMessage() => this.ToMessage(MessageType.HandShake);
+    }
+
+    public class AddrPayload : IMessage
+    {
+        public List<string> KnownIpEndPoints { get; set; }
+
+        public Message ToMessage() => this.ToMessage(MessageType.Addr);
+    }
+
+    public class Ping : IMessage
+    {
+        public Message ToMessage() => this.ToMessage(MessageType.Ping);
+    }
+
+    public class SurfaceHandShake : IMessage
+    {
+        public Message ToMessage() => this.ToMessage(MessageType.SurfaceHandShake);
+    }
+
+    public class NewTransaction : IMessage
+    {
+        public Transaction Transaction { get; set; }
+
+        public Message ToMessage() => this.ToMessage(MessageType.NewTransaction);
+
+    }
+
+    public class NewBlock : IMessage
+    {
+        public Block Block { get; set; }
+
+        public Message ToMessage() => this.ToMessage(MessageType.NewBlock);
+    }
+}
